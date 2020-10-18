aa9f63a1c21b05c3696c014db03daeaa4c66a45f
ankush.bhardwaj0@gmail.com <ankush.bhardwaj0@gmail.com>
2020-10-18 19:28:17 +0530

    improve responsiveness

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -3,7 +3,12 @@ import React from 'react';
 const About = () => {
   return(
     <div>
-      <p style={{color: '#ff7e67', fontFamily: 'Piazzolla, serif'}}>
+      <p style={{color: '#ff7e67', 
+        fontFamily: 'Piazzolla, serif',
+        maxWidth: '375px',
+        display: 'block',
+        marginLeft: 'auto',
+        marginRight: 'auto'}}>
         I am a 2nd year BBA undergrad at Ramaiyah University, Bangalore.
         I can make businesses viral over the Internet, using Tech and Digital
         Marketing.
